2db1b5498d8b623c3c6a767020b7d84910ba73ec
vihaanshah2014 <58313595+vihaanshah2014@users.noreply.github.com>
2023-09-14 18:49:33 -0400

    FINALLY DONEEEE
    LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO

--- main.js
+++ main.js
@@ -57,10 +57,6 @@ arrows.forEach((arrow, i) => {
 //TOGGLE
 
 const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(
-  ".container,.movie-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle"
-);
-
 ball.addEventListener("click", () => {
   items.forEach((item) => {
     item.classList.toggle("active");
